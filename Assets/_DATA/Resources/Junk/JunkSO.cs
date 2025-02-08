bf7accca9544d84ff5fd92ce6a063f1d78473ba8
Dung <vuquocdung04@gmail.com>
2025-02-08 15:48:59 +0700

    update readme + e26 done

---
+++ Assets/_DATA/Resources/Junk/JunkSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Junk" , menuName = "ScriptableObjs/Junk")]
+public class JunkSO : ScriptableObject
+{
+    public string junkName = "Junk";
+    public float hpMax;
+}
