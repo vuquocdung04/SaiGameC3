b03f2cfd2e7b559c0193893a60514af64e6d30bf
Dung <vuquocdung204@gmail.com>
2025-02-09 12:15:31 +0700

    e36: understand 80%

--- Assets/_DATA/Item/Inventory/ItemPickupable.cs
+++ Assets/_DATA/Item/Inventory/ItemPickupable.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CircleCollider2D))]
-public class ItemPickupable : LoadAutoComponents
+public class ItemPickupable : JunkAbstract
 {
     [SerializeField] protected CircleCollider2D _circleCollider2D;
+    public static ItemCode String2ItemCode(string itemName)
+    {
+        return (ItemCode)System.Enum.Parse(typeof(ItemCode), itemName);
+    }
 
     protected override void LoadComponents()
     {
@@ -19,5 +23,16 @@ public class ItemPickupable : LoadAutoComponents
         _circleCollider2D = GetComponent<CircleCollider2D>();
         _circleCollider2D.isTrigger = true;
         _circleCollider2D.radius = 0.05f;
+    }
+
+    public virtual ItemCode GetItemCode()
+    {
+        return ItemPickupable.String2ItemCode(transform.parent.name);
+    }
+
+    // pool item
+    public virtual void Picked()
+    {
+        this.junkCtrl.JunkDespawn.DespawnObj();
     }
 }
