01c2a63dab709d972fd262603f9be5ba38ade2fb
Dung <vuquocdung204@gmail.com>
2025-02-13 20:28:43 +0700

    e69

--- Assets/_DATA/Item/ItemDropTest.cs
+++ Assets/_DATA/Item/ItemDropTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,9 @@ using UnityEngine;
 public class ItemDropTest : LoadAutoComponents
 {
     public JunkCtrl junkCtrl;
+    public int dropCount = 0;
 
+    public List<ItemDropCount> dropCountItems= new List<ItemDropCount>();
     protected virtual void Start()
     {
         InvokeRepeating(nameof(Droping),2,1);
@@ -13,9 +16,32 @@ public class ItemDropTest : LoadAutoComponents
 
     protected virtual void Droping()
     {
+        this.dropCount++;
         Vector3 dropPos = transform.position;
         Quaternion dropRot = transform.rotation;
 
-        ItemDropSpawner.Instance.Drop(this.junkCtrl.ShootableObject.dropList, dropPos,dropRot);
+        List<ItemDropRate> dropItems = ItemDropSpawner.Instance.Drop(this.junkCtrl.ShootableObject.dropList, dropPos,dropRot);
+        ItemDropCount itemDropCount;
+        foreach (ItemDropRate itemDropRate in dropItems)
+        {
+            itemDropCount = this.dropCountItems.Find(i => i.itemName == itemDropRate.itemSO.itemName);
+            if(itemDropCount == null)
+            {
+                itemDropCount = new ItemDropCount();
+                //itemS0 = itemprofileSO, quan li item
+                itemDropCount.itemName = itemDropRate.itemSO.itemName;
+                this.dropCountItems.Add(itemDropCount);
+            }
+
+            itemDropCount.count++;
+            itemDropCount.rate =(float)Math.Round((float)itemDropCount.count / this.dropCount,2);
+        }
     }
+}
+[Serializable]
+public class ItemDropCount
+{
+    public string itemName;
+    public int count;
+    public float rate;
 }
