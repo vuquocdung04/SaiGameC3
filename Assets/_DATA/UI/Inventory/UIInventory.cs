37adc7c2678020a9e2c48eb78a1cfded1933d35d
Dung <vuquocdung04@gmail.com>
2025-02-14 17:05:44 +0700

    e81

---
+++ Assets/_DATA/UI/Inventory/UIInventory.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventory : LoadAutoComponents
+{
+    static UIInventory instance;
+    public static UIInventory Instance => instance;
+    protected bool isOpen = false; 
+    protected override void Awake()
+    {
+        if (UIInventory.instance != null) Debug.LogError("Only 1 BulletSpawner allow to exist");
+        UIInventory.instance = this;
+    }
+
+    protected virtual void Start()
+    {
+        this.Close();
+    }
+
+    // ki thuat double click la close 
+    public virtual void Toggle()
+    {
+        this.isOpen = !this.isOpen;
+        if (this.isOpen) this.Open();
+        else this.Close();
+    }
+
+    public virtual void Open()
+    {
+        gameObject.SetActive(true);
+        this.isOpen = true; 
+    }
+
+    public virtual void Close()
+    {
+        gameObject.SetActive(false);
+        this.isOpen =false;
+    }
+}
